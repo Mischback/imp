9fac2025c7ad800e0339738ce02da862ff8865e4
Mischback <dev@mischback.de>
2021-10-20 00:22:52 +0200

    Read config file with cosmiconfig

--- src/lib/configure.ts
+++ src/lib/configure.ts
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 
 /* library imports */
+import { cosmiconfig } from "cosmiconfig";
+import { CosmiconfigResult } from "cosmiconfig/dist/types";
 import { getopt } from "stdio";
 import { Config } from "stdio/dist/getopt";
 
@@ -9,6 +11,27 @@ import { ImpConfigureError } from "./errors";
 import { logger } from "./logging";
 
 /* *** INTERNAL CONSTANTS *** */
+
+class ImpConfigureCosmiconfError extends ImpConfigureError {
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+function cosmiconfigWrapper(
+  configFile: string | boolean,
+  clearCache: boolean
+): Promise<CosmiconfigResult> {
+  const explorer = cosmiconfig("imp");
+
+  if (clearCache === true) explorer.clearCaches();
+
+  if (configFile !== false) {
+    return explorer.load(configFile as string);
+  } else {
+    return explorer.search();
+  }
+}
 
 export function getConfig(
   argv: string[],
@@ -28,9 +51,49 @@ export function getConfig(
         new ImpConfigureError("Could not parse command line parameters")
       );
 
-    // FIXME: just for successful compilation/linting, remove later!
-    logger.debug(cmdLineParams);
+    cosmiconfigWrapper(
+      cmdLineParams.configFile as string | boolean,
+      cmdLineParams.debug as boolean
+    )
+      .then((result) => {
+        if (result === null) {
+          return reject(
+            new ImpConfigureError("Could not find configuration object")
+          );
+        }
 
-    return resolve();
+        /* This check was added due to the api specification of cosmiconf, but
+         * actually "empty results" are already catched by the if-block above.
+         * TODO: Check when this condition may be true, remove when possible.
+         */
+        if (result?.isEmpty === true) {
+          logger.debug(
+            "Cosmiconfig resolved to an empty configuration object..."
+          );
+          logger.debug(
+            `Cosmiconfig read this config file: "${result?.filepath}"`
+          );
+          return reject(
+            new ImpConfigureError("Configuration object must not be empty")
+          );
+        }
+
+        logger.debug(`Read configuration from: "${result?.filepath}"`);
+        logger.debug(result?.config);
+        return resolve();
+      })
+      .catch((err) => {
+        /* Cosmiconf does not throw/reject custom errors, so the actual error
+         * is "catched" and then re-thrown / re-rejected with an module-specific
+         * error class and message.
+         * The original error is available in debug mode.
+         */
+        logger.debug(err);
+        return reject(
+          new ImpConfigureCosmiconfError(
+            "Error during cosmiconf operation. Activate debug mode for details!"
+          )
+        );
+      });
   });
 }
