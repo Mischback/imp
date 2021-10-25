47b97578e77635a1b3c462986ccbe429bafe65ac
Mischback <dev@mischback.de>
2021-10-25 20:11:48 +0200

    Add comments in errors.ts

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -1,5 +1,8 @@
 // SPDX-License-Identifier: MIT
 
+/**
+ * Base class for all module-related errors.
+ */
 export class ImpError extends Error {
   constructor(message: string) {
     super(message);
