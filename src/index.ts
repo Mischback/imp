be1f0c89209c7a161f38d3e80dbb98919a57bb07
Mischback <dev@mischback.de>
2021-10-28 19:50:28 +0200

    Add config types to default exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 export { impMain } from "./main";
+export { TargetConfig, FormatConfig, ImpConfig } from "./lib/configure";
 export { ImpError } from "./lib/errors";
 export {
   SharpRunner,
